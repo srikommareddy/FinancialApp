b91cb57eaf17aacca93260a1b8806a0e6e82db1b
Srinivasa Reddy Kommareddy <kommareddy1970@gmail.com>
2026-06-05 15:50:39 +0530

    Update ingestor.py

--- modules/ingestor.py
+++ modules/ingestor.py
@@ -16,7 +16,7 @@ from io import BytesIO
 import tempfile, os
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 
